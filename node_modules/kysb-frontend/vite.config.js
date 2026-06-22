b0d2c6d79f35e88b9b7d4fdeac2bf88b1ae554e4
zafarazlay <47980191+zafarazlay@users.noreply.github.com>
2026-06-22 11:08:57 +0500

    chore: setup github pages deployment with github actions

--- node_modules/kysb-frontend/vite.config.js
+++ node_modules/kysb-frontend/vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default {
+  base: '/KYSB-fitness-center-management-system/',
   plugins: [react()],
   resolve: {
     alias: {
